b429932eeecb2708d3b3a584f7924594bd8cfd3e
joaodacaixa <joaodacaixa@gmail.com>
2025-12-22 13:41:01 -0300

    ajuste appserviceproveider
    ajuste appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 // use illuminate\Support\Facades\Route; para acesar configs de rotas
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production' || env('FORCE_HTTPS')) {
+        URL::forceScheme('https');
+    }
         // constraints globais, por exemplo em uma rota que possua vairavel $id 
         //Route::pattern('id','[0-9]+'); ou
         //Route::pattern('id','[a-zA-Z]+');
