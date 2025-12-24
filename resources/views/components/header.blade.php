edd6946caf10a40047b1dbf3d014432510307b08
joaodacaixa <joaodacaixa@gmail.com>
2025-12-24 09:52:50 -0300

    ajustando funcionamento menu mobile

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -31,46 +31,34 @@
             @endauth
         </nav>
 
-        <button @click.stop="open=!open"
-            id="hamburger"
-            class="text-white md:hidden flex items-center">
-                =
-           </button>
+            <button
+             @click="open = !open"
+             class="md:hidden text-white text-2xl">
+             ☰
+            </button>
         </div>
 
-        <!-- Mobile -->
-        <nav x-show="open" @click.outside="open=false" id ="mobile-menu"
-        class="md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2">
+     <!-- Mobile -->
+    <nav
+        x-show="open"
+        x-transition
+        x-cloak
+        class="md:hidden bg-blue-900 text-white mt-5 pb-4 space-y-2"
+    >
+        <x-nav-link url="/jobs" :mobile="true" @click="open=false">All Jobs</x-nav-link>
 
-        <x-nav-link url="/jobs" :active="request()->is('jobs')" :mobile="true">All Jobs</x-nav-link>
         @auth
-        <x-nav-link url="/jobs/saved" :active="request()->is('jobs')" :mobile="true">Saved Jobs</x-nav-link>
-         <x-nav-link url="/dashboard" :active="request()->is('jobs')" :mobile="true">Dashboard</x-nav-link>
-        <x-logout-button />
-         <x-button-link url="/jobs/create" :block="true">Create Job</x-button-link> 
+            <x-nav-link url="/jobs/saved" :mobile="true" @click="open=false">Saved Jobs</x-nav-link>
+            <x-nav-link url="/dashboard" :mobile="true" @click="open=false">Dashboard</x-nav-link>
+
+            <x-logout-button @click="open=false" />
+
+            <x-button-link url="/jobs/create" @click="open=false">
+                Create Job
+            </x-button-link>
         @else
-        <x-nav-link url="/login" :active="request()->is('jobs')" :mobile="true">Login</x-nav-link>
-        <x-nav-link url="/register" :active="request()->is('jobs')" :mobile="true">Register</x-nav-link>
+            <x-nav-link url="/login" :mobile="true" @click="open=false">Login</x-nav-link>
+            <x-nav-link url="/register" :mobile="true" @click="open=false">Register</x-nav-link>
         @endauth
-        <!--    <a href = "{{url('/dashboard')}}" 
-                class= "block text-white hover:underline py-2">
-                <i class="fa-solid fa-gauge"></i>
-
-                Dashboard 
-                </a>
-
-                <a href = "{{url('jobs/create')}}" 
-                class= "block px-4 py-2 
-                bg-yellow-500 hover:bg-yellow-600 text-black">
-                Create Job 
-                </a>
-        -->
-                
-
-                <!--
-                <a href="{{url('/jobs/create')}}" class="block px-4 py-2 bg-yellow-500
-                hover:bg-yellow-600 text-black">Crate Job
-                </a>
-                -->
-        </nav>
+    </nav>
 </header>
