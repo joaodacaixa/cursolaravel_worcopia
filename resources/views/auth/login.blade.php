594fb90400dacf8b4cd67d09eb81b80233249e89
joaodacaixa <joaodacaixa@gmail.com>
2025-12-26 13:03:39 -0300

    Acerto de itens na tela de login#

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <div class="bg-white rounded-lg shadow-md w-full md:max-w-xl mx-auto mt-12 p-8 py-12">
-        <h2 class="text-4xl text-center font-bold md-4">Register</h2>
+        <h2 class="text-4xl text-center font-bold md-4">Login</h2>
         <form method="POST" action="{{route('login.authenticate')}}">
         @csrf
                 <x-input-link id="email" name="email"  label="Email adress" value=""/>
@@ -10,7 +10,7 @@
 
         <p class="mt text-gray-500">
             Do not have an account?
-            <a class="text-blue-500" href="{{route('register')}}">Login</a>
+            <a class="text-blue-500" href="{{route('register')}}">Register</a>
         </p>
         </form>
     </div>
