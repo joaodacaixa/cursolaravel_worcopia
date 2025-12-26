4d98ed0680d76f555d3c2080d20be2fe54d1dd35
joaodacaixa <joaodacaixa@gmail.com>
2025-12-26 13:12:47 -0300

    corrigindo erro de digitacao no component bottom

--- resources/views/components/bottom.blade.php
+++ resources/views/components/bottom.blade.php
@@ -1,7 +1,7 @@
 
 @props ([
  'message1' =>'Looking to hire?',
- 'message2'=>'Post your job listing naow and find the perfect candidate.' ])
+ 'message2'=>'Post your job listing now and find the perfect candidate.' ])
 <section class="container mx-auto my-6">
     <div class="bg-blue-800 text-white rounded p-4 flex items-center
     justify-between flex-col md:flex-row gap-4">
